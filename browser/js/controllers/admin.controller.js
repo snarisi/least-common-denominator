01d4e1b0eabb05a188036a73e8fa5ebb1a1d79dd
Matt Bryan <mpbryan90@gmail.com>
2016-01-05 15:24:59 -0500

    added glyphicons, shortened url, etc

--- browser/js/controllers/admin.controller.js
+++ browser/js/controllers/admin.controller.js
@@ -1,9 +1,17 @@
-app.controller('AdminCtrl', function ($scope, Group, $stateParams, $window) {
+app.controller('AdminCtrl', function ($scope, Group, $stateParams, $window, $http) {
+	$scope.shortenUrl = function(longUrl) {
+		$http.post("https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyASp1fDSqKGLmup1oTPPBU6TZpH8FpisY8", {
+			longUrl: longUrl
+		})
+		.then(function(result){
+			$scope.shortUrl = result.data.id;
+		})
+	}
     Group.getGroup($stateParams.id)
     .then(function(result){
     	$scope.group = result;
-    	console.log(window.location.host);
     	$scope.selectorUrl = "http://" + $window.location.host + "/#/group/" + result._id + "/selector";
+    	$scope.shortenUrl($scope.selectorUrl);
     })
     $scope.search = Group.search;
 });
