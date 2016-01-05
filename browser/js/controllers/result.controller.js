aef090bac7db891b71b52728f1a8d75adf01ce21
Sam Narisi <snarisi@gmail.com>
2016-01-05 14:52:54 -0500

    Added google map

--- browser/js/controllers/result.controller.js
+++ browser/js/controllers/result.controller.js
@@ -1,4 +1,21 @@
 app.controller('ResultCtrl', function ($scope, $stateParams, Group, currentGroup) {
+    var initializeMap = function (center) {
+        var latLng = {
+            lat: center.latitude,
+            lng: center.longitude
+        };
+        var mapDiv = document.getElementById('map-div');
+        var map = new google.maps.Map(mapDiv, {
+            center: latLng,
+            scrollwheel: false,
+            zoom: 16
+        });
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+        });
+    };
+
     $scope.loaded = false;
     $scope.group = currentGroup;
     console.log($scope.group);
@@ -7,5 +24,7 @@ app.controller('ResultCtrl', function ($scope, $stateParams, Group, currentGroup
             $scope.results = results
             $scope.topResult = results[0];
             $scope.loaded = true;
+            initializeMap($scope.topResult.location.coordinate);
         });
+
 });
